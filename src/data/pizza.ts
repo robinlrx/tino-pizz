a6c7fa4dfe40e599708fafc931db35b899ca907a
robinlrx <robin@monkey-lab.io>
2026-08-22 20:15:34 +0200

    add pizza data and component

---
+++ src/data/pizza.ts
@@ -0,0 +1,192 @@
+export type Pizza = {
+	name: string;
+	ingredients: string;
+	price26: string;
+	price31: string;
+};
+
+export const tomatoPizzas = [
+	{
+		"name": "Marguarita",
+		"ingredients": "Tomates, mozzarella, olives",
+		"price26": "8,00 €",
+		"price31": "10,00 €"
+	},
+	{
+		"name": "La Chorizo",
+		"ingredients": "Tomates, mozzarella, chorizo, olives",
+		"price26": "11,50 €",
+		"price31": "13,50 €"
+	},
+	{
+		"name": "Lauratino",
+		"ingredients": "Tomates, mozzarella, jambon, olives",
+		"price26": "11,50 €",
+		"price31": "13,50 €"
+	},
+	{
+		"name": "Napolitaine",
+		"ingredients": "Tomates, mozzarella, oignons, anchois, câpres, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Bolognaise",
+		"ingredients": "Tomates, mozzarella, sauce bolognaise, viande hachée, olives",
+		"price26": "11,50 €",
+		"price31": "13,50 €"
+	},
+	{
+		"name": "Calzone soufflée",
+		"ingredients": "Tomates ou crème fraîche, mozzarella, jambon, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Reine",
+		"ingredients": "Tomates, mozzarella, jambon, champignons, olives",
+		"price26": "12,00 €",
+		"price31": "14,00 €"
+	},
+	{
+		"name": "Végétarienne",
+		"ingredients": "Tomates, mozzarella, champignons, tomate fraîche, oignons, poivrons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "4 Saisons",
+		"ingredients": "Tomates, mozzarella, chorizo, champignons, poivrons, oignons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Neptune",
+		"ingredients": "Tomates, mozzarella, thon, anchois, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Orientale",
+		"ingredients": "Tomates, mozzarella, merguez, œuf, oignons, poivrons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Vésuve",
+		"ingredients": "Tomates, mozzarella, chorizo, chèvre, oignons, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Spéciale Chef",
+		"ingredients": "Tomates, mozzarella, jambon de pays, crème fraîche, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Nordique",
+		"ingredients": "Tomates, mozzarella, saumon fumé, crème fraîche, aneth, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Américaine",
+		"ingredients": "Tomates, mozzarella, bacon, lardons, chèvre, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "4 Fromages",
+		"ingredients": "Tomates, mozzarella, bleu, chèvre, camembert, raclette, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Royale",
+		"ingredients": "Tomates, mozzarella, champignons, jambon, lardons, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Florentino",
+		"ingredients": "Tomates, mozzarella, merguez, chorizo, poivrons, oignons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Burger",
+		"ingredients": "Tomates, mozzarella, viande hachée, cheddar, tomate fraîche, sauce burger, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Hawaïenne",
+		"ingredients": "Tomates, mozzarella, bacon, ananas, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "La Valentina",
+		"ingredients": "Tomates, mozzarella, jambon de pays, roquette, parmesan, tomates cerises",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	}
+];
+
+export const creamPizzas = [
+	{
+		"name": "Roma",
+		"ingredients": "Crème fraîche, mozzarella, chèvre, miel, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Sicilienne",
+		"ingredients": "Crème fraîche, mozzarella, pomme de terre, merguez, camembert, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Tartiflette",
+		"ingredients": "Crème fraîche, mozzarella, pomme de terre, raclette, lardons, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Tino",
+		"ingredients": "Crème fraîche, mozzarella, jambon, chèvre, champignons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Trembladaise",
+		"ingredients": "Crème fraîche, mozzarella, pomme de terre, lardons, bleu, oignons, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Venise",
+		"ingredients": "Crème fraîche, mozzarella, chorizo, chèvre, merguez, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "L\u0027indienne",
+		"ingredients": "Crème fraîche, mozzarella, curry, poulet, poivrons, œuf, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Kebab",
+		"ingredients": "Crème fraîche, mozzarella, viande kebab, pomme de terre, oignons, sauce kebab, olives",
+		"price26": "12,00 €",
+		"price31": "14,80 €"
+	},
+	{
+		"name": "Garguantua",
+		"ingredients": "Crème fraîche, mozzarella, pomme de terre, champignons, chorizo, lardons, jambon, œuf, olives",
+		"price26": "14,50 €",
+		"price31": "16,50 €"
+	}
+];
