6213b41be48f8232207f4f1e870456c2e5759872
robinlrx <emailderobin@gmail.com>
2026-08-23 15:55:45 +0200

    clean repo - remove decapcms

---
+++ src/data/tour.ts
@@ -0,0 +1,32 @@
+export const tour = [
+	{
+		day: 'Mardi',
+		adress: 'Camping Mus de Loup - Ronce les Bains',
+		hourStart: '18:00',
+		hourEnd: '22:00',
+	},
+	{
+		day: 'Mercredi',
+		adress: 'Camping Mus de Loup - Ronce les Bains',
+		hourStart: '18:00',
+		hourEnd: '22:00',
+	},
+	{
+		day: 'Jeudi',
+		adress: 'Camping Mus de Loup - Ronce les Bains',
+		hourStart: '18:00',
+		hourEnd: '22:00',
+	},
+	{
+		day: 'Vendredi',
+		adress: 'Camping Mus de Loup - Ronce les Bains',
+		hourStart: '18:00',
+		hourEnd: '22:00',
+	},
+	{
+		day: 'Samedi',
+		adress: 'Camping Mus de Loup - Ronce les Bains',
+		hourStart: '18:00',
+		hourEnd: '22:00',
+	},
+]
