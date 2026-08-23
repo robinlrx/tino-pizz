6070c9badcf826c58473f573e142c0d90bcf849b
robinlrx <emailderobin@gmail.com>
2026-08-23 16:49:52 +0200

    init sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sanity(), react()]
 });
